d6bbdccbaa9f12c1800483c7a5f29e57a4989343
jaem1n207 <jaem1n207@naver.com>
2019-10-08 17:22:31 +0900

    [Modify] Login css
    로그인화면이 짤리는 오류 수정

--- jelly_my_pet/src/components/Post/Post.js
+++ jelly_my_pet/src/components/Post/Post.js
@@ -1,10 +1,7 @@
-import React from "react";
-import { Form, Input, Button, Card, Icon, Avatar } from "antd";
+import React, { Fragment } from "react";
 import { withRouter } from "react-router-dom";
 
 const dummy = {
-  isLoggedIn: true,
-  imagePaths: [],
   mainPosts: [
     {
       User: {
@@ -18,68 +15,7 @@ const dummy = {
 };
 
 const Post = () => {
-  return (
-    <Card>
-      <div>
-        {dummy.isLoggedIn && (
-          <Form style={{ marginBottom: "20px" }} encType="multipart/form-data">
-            <Input.TextArea
-              maxLength={140}
-              placeholder="무슨 생각을 하고 계신가요?"
-            />
-            <div>
-              <input type="file" multiple hidden />
-              <Button>이미지 업로드</Button>
-              <Button
-                type="primary"
-                style={{ float: "right" }}
-                htmlType="submit"
-              >
-                짹짹
-              </Button>
-            </div>
-            <div>
-              {dummy.imagePaths.map((v, i) => {
-                return (
-                  <div key={v} style={{ display: "inline-block" }}>
-                    <img
-                      src={"http://localhost:3000" + v}
-                      style={{ width: "200px" }}
-                      all={v}
-                    />
-                    <div>
-                      <Button>제거</Button>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </Form>
-        )}
-        {dummy.mainPosts.map(c => {
-          return (
-            <Card
-              key={+c.createdAt}
-              cover={c.img && <img alt="example" src={c.img} />}
-              actions={[
-                <Icon type="retweet" key="retweet" />,
-                <Icon type="heart" key="heart" />,
-                <Icon type="message" key="message" />,
-                <Icon type="ellipsis" key="ellipsis" />
-              ]}
-              extra={<Button>팔로우</Button>}
-            >
-              <Card.Meta
-                avatar={<Avatar>{c.User.nickname[0]}</Avatar>}
-                title={c.User.nickname}
-                description={c.content}
-              />
-            </Card>
-          );
-        })}
-      </div>
-    </Card>
-  );
+  <Fragement>div.</Fragement>;
 };
 
 export default withRouter(Post);
