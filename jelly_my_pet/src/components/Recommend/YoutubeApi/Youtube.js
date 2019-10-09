89531783fcc715e825c458e5aa5ba27fdcb7bbe0
JIN <dhehdlflrhks@naver.com>
2019-10-09 20:45:06 +0900

    Set up the "Recommend page" structure

---
+++ jelly_my_pet/src/components/Recommend/YoutubeApi/Youtube.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Youtube.css';
+
+const YoutubeUrl = {
+    Url : "#",
+    title : "",
+    status : "",
+    getYoutubeUrl() {
+        
+    }
+}
+
+
+const Youtube = () => {
+    
+
+    return (
+        <>
+          
+        </>
+    )
+}
+
+export default Youtube;
