bd35da420f90b8455c43d241723578bab47230f0
JIN <dhehdlflrhks@naver.com>
2019-10-15 10:44:49 +0900

    help me!

--- jelly_my_pet/src/components/Recommend/YoutubeApi/Youtube.js
+++ jelly_my_pet/src/components/Recommend/YoutubeApi/Youtube.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import axios from "axios";
 import './Youtube.css';
-import { SERVER } from "config/config.json";
+import {SERVER} from '../../../config/config.json';
 
+class Youtube extends Component {
 
-const Youtube = () => {
-    const getYoutube = () => {
-        try {
-          return axios.get(`${SERVER}/rcmnd`);
-        } catch (error) {
-            console.error(error)
-          }
-      };
+  state = {
+    url : [],
+    title : [],
+  };
   
-    const getYoutubeUrl = () => {
-        let data = {
-            url: ""
-        };
-        const getUrl = getYoutube()
-            .then(response => {
-                data = response.data.url;
-                console.log(data);
-            })
-            .catch(error => {
-              console.log(error);
-            })
-        
-    };
+  async getYoutubeinfo () {
+    try {
+      const info = await axios.get(`${SERVER}/rcmnd`);
+      console.log(info);
+      this.setState({
+        title: info.data.title,
+        url: info.data.url,
+      });
+    }catch (error){
+      console.log(error);
+    }
+      
+  }
+
+  render() {
+    
+    this.getYoutubeinfo();
     return (
       <>
-        <button type="button" onClick={getYoutubeUrl}>Click Me!</button> 
+        <div className="yo-main">
+          <div className="yo-youtubelist">
+            <div className="yo-youtube">
+              <iframe src={this.state.url}/>
+            </div>
+            <div className="yo-youtube">
+              <iframe src={this.state.url}/>
+            </div>
+            <div className="yo-youtube">
+              <iframe src={this.state.url}/>
+            </div>
+            <div className="yo-youtube">
+              <iframe src={this.state.url}/>
+            </div>
+          </div>
+        </div>
       </>
-    )
- }
+    );
+  }
+}
 
 export default Youtube;
