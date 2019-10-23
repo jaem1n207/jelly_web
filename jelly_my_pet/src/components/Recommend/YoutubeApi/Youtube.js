43c923251f89c0a15778d5979e771679e75d5705
JIN <dhehdlflrhks@naver.com>
2019-10-23 11:17:48 +0900

    add YoutubeApi

--- jelly_my_pet/src/components/Recommend/YoutubeApi/Youtube.js
+++ jelly_my_pet/src/components/Recommend/YoutubeApi/Youtube.js
@@ -6,46 +6,48 @@ import {SERVER} from '../../../config/config.json';
 class Youtube extends Component {
 
   state = {
-    url : [],
+    video_id : [],
     title : [],
   };
   
   async getYoutubeinfo () {
     try {
-      const info = await axios.get(`${SERVER}/rcmnd`);
+      const info = await axios.get(`http://10.80.162.116:8080/rcmnd`);
       console.log(info);
       this.setState({
         title: info.data.title,
-        url: info.data.url,
+        video_id: info.data.video_id,
       });
     }catch (error){
       console.log(error);
     }
-      
   }
 
-  render() {
-    
+  componentDidMount(){
     this.getYoutubeinfo();
+  };
+
+  render() {
     return (
       <>
         <div className="yo-main">
           <div className="yo-youtubelist">
             <div className="yo-youtube">
-              <iframe src={this.state.url}/>
+              <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+this.state.video_id[0]} frameborder="0"/>
             </div>
             <div className="yo-youtube">
-              <iframe src={this.state.url}/>
+              <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+this.state.video_id[1]} frameborder="0"/>
             </div>
             <div className="yo-youtube">
-              <iframe src={this.state.url}/>
+              <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+this.state.video_id[2]} frameborder="0"/>
             </div>
             <div className="yo-youtube">
-              <iframe src={this.state.url}/>
+              <iframe width="560" height="315" src={"https://www.youtube.com/embed/"+this.state.video_id[3]} frameborder="0"/>
             </div>
           </div>
         </div>
       </>
+    
     );
   }
 }
